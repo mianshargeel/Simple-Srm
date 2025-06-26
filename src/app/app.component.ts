f49da32b1f3b19e21c3c0612a49c0704fa092a32
mian shargeel <call2poma@gmail.com>
2025-06-26 20:19:42 +0200

    Created Toolbar and sidenav with some links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MainContentComponent } from './main-content/main-content/main-content.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MainContentComponent,
+    MatSidenavModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
