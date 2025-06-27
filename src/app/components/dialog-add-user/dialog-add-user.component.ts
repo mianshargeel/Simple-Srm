dc678e546eefb3e94762587040397d454e89e0e1
mian shargeel <call2poma@gmail.com>
2025-06-27 12:51:22 +0200

    Created some fields to add-user-dialog using Angular-Material

--- src/app/components/dialog-add-user/dialog-add-user.component.ts
+++ src/app/components/dialog-add-user/dialog-add-user.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers: [
+    provideNativeDateAdapter() 
+  ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
-
+  value = 'Clear me';
 }
