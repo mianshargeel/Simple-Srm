7310fa4ed7accec01753cf5f90f6f61287e0df6f
mian shargeel <call2poma@gmail.com>
2025-06-27 10:26:57 +0200

    Created new component to create dialog-add-user via Angular-Material

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog, MatDialogRef, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
@@ -11,5 +13,11 @@ import {MatTooltipModule} from '@angular/material/tooltip';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  constructor(public dialog: MatDialog) { }
+  
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent); //open component-DialogOverviewExampleDialog
+  }
 
 }
